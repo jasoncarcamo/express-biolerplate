d497bc762baaf6a834711c3ca01dd841104c94b2
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2019-06-11 16:27:16 -0400

    update

---
+++ test/app.spec.js
@@ -0,0 +1,8 @@
+
+const app = require('../src/app');
+
+describe('App', ()=>{
+    it('GET / responds with 200 containing "hello world!"', ()=>{
+        return supertest(app).get('/').expect(200, 'Hello, world!')
+    })
+})
